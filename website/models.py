86efcfc97d5574c59ed95ae9bd4071440b815d1e
NavenRajkumar <naven1999@gmail.com>
2017-12-16 18:01:42 +0530

    Added user model

--- website/models.py
+++ website/models.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    user_name = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+
+    class Meta:
+        db_table = 'users'
+
+        
