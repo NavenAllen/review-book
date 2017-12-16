04c5c76f840b205a1c162ea289b9a23a9ac56b76
NavenRajkumar <naven1999@gmail.com>
2017-12-16 21:12:15 +0530

    Added authorisation

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('auth', views.auth, name='auth'),
+    path('register', views.register, name='register'),
+    path('home', views.home, name='home'),
+    path('search', views.search, name='search')
 ]
--- website/views.py
+++ website/views.py
@@ -2,9 +2,44 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.template import loader
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+
+from .models import User
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    if(request.session.get('user')):
+        return render(request, 'website/home.html')
+    else:
+        return render(request, 'website/index.html')
+
+def auth(request):
+    try:
+        selected_user = User.objects.get(user_name=request.POST['user_name'], password=request.POST['password'])
+        request.session['user'] = selected_user.user_name;
+    except (KeyError, User.DoesNotExist):
+        return render(request, 'website/index.html', {
+            'error_message': "Invalid Credentials.",
+        })
+    else:
+        return HttpResponseRedirect(reverse('home'))
+
+def register(request):
+     newUser = User(user_name=request.POST['user_name'], password=request.POST['password'])
+     newUser.save()
+     return HttpResponseRedirect(reverse('index'))
+
+def home(request):
+    return render(request, 'website/home.html')
+
+def search(request):
+    searched_user = User.objects.get(user_name=request.GET['search_query'])
+    template = loader.get_template('polls/profile.html')
+    context = {
+        'user': searched_user,
+    }
+    return HttpResponse(template.render(context, request))
+
