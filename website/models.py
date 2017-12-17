eb01025a0a6d5196a73ff752a6a68c37c30deb8b
SridharPandian <sripan172@gmail.com>
2017-12-17 06:50:22 +0530

    added new models

--- website/models.py
+++ website/models.py
@@ -18,11 +18,29 @@ class Review(models.Model):
     review_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="creator")
     upvotes = models.IntegerField(default=0)
     downvotes = models.IntegerField(default=0)
-    title = models.CharField(max_length=15)
+    title = models.CharField(max_length=30)
     text = models.CharField(max_length=240)
 
     class Meta:
         db_table = 'reviews'
+
+class Gmembers:
+    _id = models.AutoField(primary_key=True)
+    members = models.ForeignKey(User, on_delete=models.CASCADE, related_name="group_members")
+    
+    class Meta:
+        db_table = 'gmembers'
+
+class Greview:
+    _id = models.AutoField(primary_key=True)
+    g_upvotes = models.IntegerField(default=0)
+    g_downvotes = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'greviews'
+
+    
+    
 
 
 
